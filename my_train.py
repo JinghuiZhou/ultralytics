b91b43d3d8fbd50c9e6d142e1945fb84a034cf1f
philuto <961031769@qq.com>
2024-04-29 13:45:51 +0800

    '实现了pt模型不落盘进行模型验证'

--- my_train.py
+++ my_train.py
@@ -4,4 +4,5 @@ model = YOLO('YOLOv8n-seg.pt')
 
 if __name__ == '__main__':
     results = model.train(data=r'E:\Company\AITrain\convert_data\area\dataset.yaml',epochs=20)
+
     # model.export(format='onnx', simplify=True, dynamic=True)
